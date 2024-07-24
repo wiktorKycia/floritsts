493acccfd22d0785e7f573d76a8dd47c30dc833d
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-07-24 17:20:06 +0200

    add some basic components with basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-
+import Header from "./components/Header/Header.jsx"
+import Main from "./components/Main/Main.jsx"
+import Footer from "./components/Footer/Footer.jsx"
 
 function App() {
-
+    return(
+        <>
+            <Header></Header>
+            <Main></Main>
+            <Footer></Footer>
+        </>
+        
+    )
 }
 
 export default App
