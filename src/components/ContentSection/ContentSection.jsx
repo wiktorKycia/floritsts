9c544d91062f36437dab43816f791b5439b4268d
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-07-25 13:12:13 +0200

    add contentSection component to show more on the page than just the quote

---
+++ src/components/ContentSection/ContentSection.jsx
@@ -0,0 +1,12 @@
+
+
+function ContentSection(props)
+{
+    return(
+        <section>
+            <h2>props.title</h2>
+            <div className="container">props.children</div>
+        </section>
+    )
+}
+export default ContentSection;
