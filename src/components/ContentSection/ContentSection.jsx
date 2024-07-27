ec7dd34bcbb86d03b665c54d0773653f8ff06205
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-07-27 17:33:29 +0200

    define proptypes for ContentSection

--- src/components/ContentSection/ContentSection.jsx
+++ src/components/ContentSection/ContentSection.jsx
@@ -1,4 +1,5 @@
 import styles from './ContentSection.module.css';
+import PropTypes from 'prop-types';
 
 function ContentSection(props)
 {
@@ -8,5 +9,11 @@ function ContentSection(props)
             <div className={styles.container}>{props.children}</div>
         </section>
     )
+}
+ContentSection.propTypes = {
+    title: PropTypes.string
+}
+ContentSection.defaultProps = {
+    title: "Bestsellers of 2024"
 }
 export default ContentSection;
